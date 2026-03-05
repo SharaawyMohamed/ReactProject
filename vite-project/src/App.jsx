b9cd6d26cab7adcfe8cf1ecd71c0a917b7f1ac12
Sharawy <sharawymohamed.a@gmail.com>
2026-03-05 18:27:41 +0200

    Initial creating

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import {About} from './Components/About/About.jsx'
+export function App() {
+
+  return (
+   <div>
+    <About/>
+    <h1>App</h1>
+    <p>This is the app page</p>
+   </div>
+  )
+}
+
+export default App
